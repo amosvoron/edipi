51def3e4f3c5f23f3d5c38e389f35d6ee148f3a3
amosvoron <amos.voron@gmail.com>
2020-03-03 10:15:49 +0100

    feat: Add finalizer

---
+++ db/stored-procedures/Finalize.sql
@@ -0,0 +1,41 @@
+USE [EDIPI]
+GO
+
+/****** Object:  StoredProcedure [dbo].[Finalize]    Script Date: 3. 03. 2020 10:15:15 ******/
+SET ANSI_NULLS ON
+GO
+
+/*
+Object:			dbo.Finalize
+Description:	Finalize the processing with setting the database state as READY.
+Note:			This procedure is called AT THE END of the dbo.ProcessTrans processing.
+				---------------------------
+				Here the OK log is written.
+				---------------------------
+*/
+
+CREATE PROCEDURE [dbo].[Finalize]
+AS
+
+SET NOCOUNT ON;
+
+-- Sets the database state as READY.
+--UPDATE dbo.[Config] SET [DatabaseState] = 0;
+EXEC dbo.SetReady;
+
+-- Re-enable the triggers.
+EXEC dbo.EnableRejectTriggers;
+
+-- Output to console.
+EXEC dbo.FastPrint '=============================================================';
+EXEC dbo.FastPrint 'THE PROCESSING COMPLETED. DATABASE IS IN READY STATE.';
+EXEC dbo.FastPrint '=============================================================';
+
+-- Write log.
+EXEC dbo.WriteLog 1, 'OK', NULL, 0;
+
+
+
+GO
+
+
