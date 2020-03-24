c48c035a209d98bd4a4e892440ee835ea91f673e
amosvoron <amos.voron@gmail.com>
2020-03-24 21:53:00 +0100

    feat: Add restore procedures

---
+++ db/stored-procedures/RestoreMembership.sql
@@ -0,0 +1,166 @@
+
+/*
+Object:			dbo.RestoreMembership
+Description:	Restore membership data for a given N interested parties.		 
+*/
+
+CREATE PROCEDURE [dbo].[RestoreMembership]
+	@N int = 10			-- how many IPs to restore
+	, @ID int = NULL	-- if given only THIS particular ID is processed
+AS
+
+SET NOCOUNT ON;
+
+BEGIN TRY
+
+	IF @N <= 0
+	BEGIN
+		PRINT 'N must be greater than zero.';
+		RETURN;
+	END;
+
+--------------------------------------------------------
+-- Select N IPs
+--------------------------------------------------------
+
+	CREATE TABLE #IDS (ID int NOT NULL PRIMARY KEY);
+
+	IF @ID IS NULL
+	BEGIN
+		INSERT INTO #IDS
+		SELECT TOP(@N) ID
+		FROM [dbo].[Restore] AS A
+		WHERE CanRestore = 1
+		ORDER BY ID;
+	END
+	ELSE BEGIN
+		INSERT INTO #IDS
+		SELECT ID
+		FROM [dbo].[Restore] AS A
+		WHERE ID = @ID AND CanRestore = 1;
+	END;
+
+	IF @@ROWCOUNT = 0
+	BEGIN
+		PRINT 'No data to restore. Check CanRestore flag in [dbo].[Restore] table.';
+		RETURN;
+	END;
+
+	-- update processing status
+	UPDATE X
+	SET X.ForProcessing = 1
+	FROM [dbo].[Restore] AS X
+	WHERE X.ID IN (SELECT ID FROM #IDS);
+
+--------------------------------------------------------
+-- Store current membership data count in Restore table
+--------------------------------------------------------
+
+	UPDATE X
+	SET X.[MembershipOldCount] = ISNULL(A.N, 0)
+	FROM [dbo].[Restore] AS X
+	LEFT OUTER JOIN (
+		SELECT AA.ID, COUNT(*) AS N
+		FROM [ipi].[IPMembership] AS AA
+		INNER JOIN #IDS AS CC ON AA.ID = CC.ID
+		GROUP BY AA.ID
+	) AS A ON X.ID = A.ID
+	WHERE EXISTS (
+		SELECT 1
+		FROM #IDS AS AA
+		WHERE AA.ID = X.ID);
+
+	UPDATE X
+	SET X.[MembershipTerritoryOldCount] = ISNULL(A.N, 0)
+	FROM [dbo].[Restore] AS X
+	LEFT OUTER JOIN (
+		SELECT AA.ID, COUNT(*) AS N
+		FROM [ipi].[IPMembership] AS AA
+		INNER JOIN [ipi].[IPMembershipTerritory] AS BB ON AA.MID = BB.MID
+		INNER JOIN #IDS AS CC ON AA.ID = CC.ID
+		GROUP BY AA.ID
+	) AS A ON X.ID = A.ID
+	WHERE EXISTS (
+		SELECT 1
+		FROM #IDS AS AA
+		WHERE AA.ID = X.ID);
+
+--------------------------------------------------------
+-- Loop through IPs and run RestoreMembershipByIP
+-- (for every IP)
+--------------------------------------------------------
+
+	EXEC dbo.FastPrint 'Processing started...';
+
+	DECLARE @Ret int;
+	DECLARE @k int = 1;
+
+	-- cursor			
+	DECLARE cur1 CURSOR LOCAL READ_ONLY FOR
+		SELECT ID FROM #IDS;	
+	OPEN cur1;
+	FETCH NEXT FROM cur1 INTO @ID;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		--EXEC dbo.FastPrint @ID;
+
+		EXEC [dbo].[RestoreMembershipByIP] @ID;
+
+		IF @k % 1000 = 0 EXEC dbo.FastPrint @k;
+		SET @k = @k + 1;
+
+		FETCH NEXT FROM cur1 INTO @ID;
+	END	
+	CLOSE cur1;
+	DEALLOCATE cur1;
+	
+END TRY
+BEGIN CATCH
+	IF XACT_STATE() != 0 ROLLBACK;	
+	DECLARE @e NVARCHAR(MAX) = 'ERROR: ' + ERROR_MESSAGE(); 
+	INSERT INTO [dbo].[RestoreLog] (ID, HeaderID, [Message]) 
+		VALUES (ISNULL(@ID, -1), -2, @e);
+	EXEC dbo.FastPrint @e;
+END CATCH;
+
+--------------------------------------------------------
+-- Finalizer
+--------------------------------------------------------
+
+-- update processing status
+UPDATE [dbo].[Restore]
+SET IsProcessing = 0
+	, ForProcessing = 0
+WHERE ForProcessing = 1;
+
+-- update new membership counters
+UPDATE X
+SET X.[MembershipNewCount] = ISNULL(A.N, 0)
+FROM [dbo].[Restore] AS X
+LEFT OUTER JOIN (
+	SELECT AA.ID, COUNT(*) AS N
+	FROM [ipi].[IPMembership] AS AA
+	INNER JOIN #IDS AS CC ON AA.ID = CC.ID
+	GROUP BY AA.ID
+) AS A ON X.ID = A.ID
+WHERE EXISTS (
+	SELECT 1
+	FROM #IDS AS AA
+	WHERE AA.ID = X.ID);
+
+UPDATE X
+SET X.[MembershipTerritoryNewCount] = ISNULL(A.N, 0)
+FROM [dbo].[Restore] AS X
+LEFT OUTER JOIN (
+	SELECT AA.ID, COUNT(*) AS N
+	FROM [ipi].[IPMembership] AS AA
+	INNER JOIN [ipi].[IPMembershipTerritory] AS BB ON AA.MID = BB.MID
+	INNER JOIN #IDS AS CC ON AA.ID = CC.ID
+	GROUP BY AA.ID
+) AS A ON X.ID = A.ID
+WHERE EXISTS (
+	SELECT 1
+	FROM #IDS AS AA
+	WHERE AA.ID = X.ID);
+
+EXEC dbo.FastPrint 'Membership restore completed.';
---
+++ db/stored-procedures/RestoreMembershipByIP.sql
@@ -0,0 +1,104 @@
+
+/*
+Object:			dbo.RestoreMembershipByIP
+Description:	Restores all membership data (IPMembership + IPMembershipTerritory)
+				for a given IP from the first to the last membership transaction.
+Note:			This procedure eliminates all current membership data for IP
+				and imports data from the raw source (dbo.Raw). No file import is needed 
+				unless the raw source is empty.				 
+*/
+
+CREATE PROCEDURE [dbo].[RestoreMembershipByIP]
+	@ID int
+AS
+
+SET NOCOUNT ON;
+
+DECLARE @IPBN char(13) = (SELECT IPBN FROM ipi.IP WHERE ID = @ID);
+
+DECLARE @HeaderID bigint = -1;
+BEGIN TRY
+
+	BEGIN TRANSACTION;
+
+	-- set processing status
+	UPDATE [dbo].[Restore]
+	SET IsProcessing = 1
+	WHERE ID = @ID;
+
+	-- remove membership data
+	DELETE FROM ipi.IPMembership
+	WHERE ID = @ID;
+
+	-- begin cursor			
+	DECLARE cur1 CURSOR LOCAL READ_ONLY FOR
+		SELECT DISTINCT HeaderID
+		FROM dbo.[Row]
+		WHERE IPBN = @IPBN
+			AND HeaderCode IN ('IPA', 'MAA', 'MAD', 'MAN', 'MAO', 'MAU')
+		ORDER BY HeaderID;
+	
+	OPEN cur1;
+
+	FETCH NEXT FROM cur1 INTO @HeaderID;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		--EXEC dbo.FastPrint @HeaderID;
+
+		-- IPA:MAN
+		EXEC dbo.Query_50 @HeaderID;
+
+		-- IPA:TMA
+		EXEC dbo.Query_60 @HeaderID;
+
+		-- MAA:MAN
+		EXEC dbo.Query_100 @HeaderID;
+			
+		-- MAA:TMA
+		EXEC dbo.Query_110 @HeaderID;
+
+		-- MAD:MAO
+		EXEC dbo.Query_380 @HeaderID;
+
+		-- MAU:MAO
+		EXEC dbo.Query_250 @HeaderID;
+
+		-- MAU:MAN
+		EXEC dbo.Query_260 @HeaderID;
+
+		-- MAU:TMA
+		EXEC dbo.Query_270_FORCED @HeaderID;
+
+		FETCH NEXT FROM cur1 INTO @HeaderID;
+	END	-- cursor
+
+	CLOSE cur1;
+	DEALLOCATE cur1;
+	-- end of cursor
+
+	-- update restore table
+	UPDATE [dbo].[Restore]
+	SET CanRestore = 0
+		, IsProcessing = 0
+	WHERE ID = @ID;
+
+	COMMIT;
+
+END TRY
+BEGIN CATCH
+	IF XACT_STATE() != 0 ROLLBACK;	
+	DECLARE @e NVARCHAR(MAX) = 'ERROR: ' + ERROR_MESSAGE(); 
+	INSERT INTO [dbo].[RestoreLog] (ID, HeaderID, [Message]) VALUES (@ID, @HeaderID, @e);
+	EXEC dbo.FastPrint @e;
+
+	-- set processing status
+	UPDATE [dbo].[Restore]
+	SET IsProcessing = 0
+	WHERE ID = @ID;
+
+	RETURN -1;    -- NOT OK
+END CATCH;
+
+RETURN 0;	-- OK
+
+
